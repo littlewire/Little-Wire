0d2d7ae1d9a368fb2bcfa7b694483609f9635f74
ihsan_kehribar <kehribar@sabanciuiniv.edu>
2012-05-09 01:40:57 +0300

    Minor doxygen fix

---
+++ computer_interface/C++/docs/html/search/all_63.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['connect',['connect',['../classlittleWire.html#ab27313730210db2f7bf6e2f080433b57',1,'littleWire']]]
+];
---
+++ computer_interface/C++/docs/html/search/all_72.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readfirmwareversion',['readFirmwareVersion',['../classlittleWire.html#abd208452c78384fa4b7323ad5038f3bd',1,'littleWire']]]
+];
