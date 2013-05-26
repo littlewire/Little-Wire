20476ce04afb93e078515f16fce5d1e1f50a84e5
cpldcpu <cpldcpu@gmail.com>
2013-05-26 21:59:08 +0200

    ws2812: String is only writte once now.

--- v1.2/firmware/source/main.c
+++ v1.2/firmware/source/main.c
@@ -1326,9 +1326,8 @@ int main(void) {
 			_delay_ms(1); // Hack: Make sure USB communication has finished before atomic block.
 				ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
 				{
-					for (i=0; i<ws2812_ptr; i+=3)
-						ws2812_sendarray_mask(&ws2812_grb,ws2812_ptr,ws2812_mask);	  //mask=1<<(data[2]&7)					
-						ws2812_ptr=0;
+					ws2812_sendarray_mask(&ws2812_grb,ws2812_ptr,ws2812_mask);	  //mask=1<<(data[2]&7)					
+					ws2812_ptr=0;
 				}				
 				jobState=0;
 			break;
